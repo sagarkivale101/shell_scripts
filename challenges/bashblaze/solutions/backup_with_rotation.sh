23e95624a4e2ccfb7e5e49525d2bfc0ecf2d538a
sagar <sagar.kivle@iauro.com>
2023-09-20 19:06:49 +0530

    added questions and ans

---
+++ challenges/bashblaze/solutions/backup_with_rotation.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+# read dir
+# echo "Current path: $(pwd)"
+
+
+# ls -lh ~/$(dir) --block-size=MB
+echo $dir
---
+++ challenges/bashblaze/solutions/solution_day1_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# echo "HELLO world"
+
+
+# ##################
+# nu=1
+
+
+# echo "Number is $nu"
+# ###################
+
+# read num1 
+# read num2
+# num3=$((num1+num2))
+# echo $num3
+
+###############################
+# printenv
+
+# echo $PWD
+###############################
+
+# ls ~/Downloads | grep '.yml\|.yaml\|.gz'
+
+#OR 
+
+# ls ~/Downloads/*.gz
